2d5505cea29e15681bebe7fc4e9cd933130ef925
Eriq Augustine <eriq.augustine@gmail.com>
2025-05-21 10:59:36 -0400

    Added a module bin for eightpuzzle.

---
+++ pacai/eightpuzzle/__main__.py
@@ -0,0 +1,10 @@
+"""
+A shortcut for the main 8 Puzzle executable: `pacai.eightpuzzle.bin`.
+"""
+
+import sys
+
+import pacai.eightpuzzle.bin
+
+if (__name__ == '__main__'):
+    sys.exit(pacai.eightpuzzle.bin.main())
