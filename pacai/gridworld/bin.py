09d6ff32dbc717ff30274044f2645a077efb47f3
Eriq Augustine <eriq.augustine@gmail.com>
2025-06-02 16:48:45 -0700

    Moved several GridWorld options to agents.

--- pacai/gridworld/bin.py
+++ pacai/gridworld/bin.py
@@ -16,13 +16,6 @@ import pacai.util.alias
 DEFAULT_BOARD: str = 'gridworld-book'
 DEFAULT_SPRITE_SHEET: str = 'gridworld'
 
-DEFAULT_EPSILON: float = 0.3
-DEFAULT_ITERATIONS: int = 10
-DEFAULT_EPISODES: int = 1
-DEFAULT_LEARNING_RATE: float = 0.5
-DEFAULT_LIVING_REWARD: float = 0.0
-DEFAULT_DISCOUNT_RATE: float = 0.9
-
 def set_cli_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     """
     Set GridWorld-specific CLI arguments.
@@ -34,44 +27,17 @@ def set_cli_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
             help = ('Select the agent type will be used (default: %(default)s).'
                     + f' Builtin agents: {pacai.util.alias.AGENT_SHORT_NAMES}.'))
 
-    # TEST - Some of these may be agent args, not CLI args.
-
-    parser.add_argument('--epsilon', dest = 'epsilon',
-            action = 'store', type = float, default = DEFAULT_EPSILON,
-            help = 'The chance of taking a random action in Q-learning (default %(default)s).')
-
-    parser.add_argument('--iterations', dest = 'iterations',
-            action = 'store', type = int, default = DEFAULT_ITERATIONS,
-            help = 'The number of rounds of value iteration (default %(default)s).')
-
-    parser.add_argument('--episodes', dest = 'episodes',
-            action = 'store', type = int, default = DEFAULT_EPISODES,
-            help = 'The number of epsiodes of the MDP to run (default %(default)s).')
-
-    parser.add_argument('--learning-rate', dest = 'learning_rate',
-            action = 'store', type = float, default = DEFAULT_LEARNING_RATE,
-            help = 'The Learning rate (default %(default)s).')
-
     parser.add_argument('--noise', dest = 'noise',
             action = 'store', type = float, default = pacai.gridworld.mdp.DEFAULT_NOISE,
-            help = 'How often actions result in unintended directions (default %(default)s).')
+            help = 'How often agent actions result in unintended movement (default %(default)s).')
 
     parser.add_argument('--living-reward', dest = 'living_reward',
-            action = 'store', type = float, default = DEFAULT_LIVING_REWARD,
+            action = 'store', type = float, default = pacai.gridworld.mdp.DEFAULT_LIVING_REWARD,
             help = 'The Reward for living for a time step (default %(default)s).')
-
-    parser.add_argument('--discount_rate', dest = 'discount_rate',
-            action = 'store', type = float, default = DEFAULT_DISCOUNT_RATE,
-            help = 'The discount rate on future (default %(default)s).')
 
     parser.add_argument('--qvalue-display', dest = 'qvalue_display',
             action = 'store_true', default = False,
             help = 'Display values, poilcies, and q-values (default %(default)s).')
-
-    # TEST - Replace with logging/debug?
-    parser.add_argument('--value-steps', dest = 'value_steps',
-            action = 'store_true', default = False,
-            help = 'Display each step of value iteration (default %(default)s).')
 
     return parser
 
@@ -89,11 +55,6 @@ def init_from_args(args: argparse.Namespace) -> tuple[dict[int, pacai.core.agent
         'name': args.agent,
         'remember_last_action': False,
         'mdp': mdp,
-        'discount_rate': args.discount_rate,
-        'iterations': args.iterations,
-        'alpha': args.learning_rate,
-        'gamma': args.discount_rate,
-        'epsilon': args.epsilon,
     }
 
     base_agent_infos: dict[int, pacai.core.agentinfo.AgentInfo] = {
