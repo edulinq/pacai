2a037d48430ac6607713c591cceea8ffd32305d8
Eriq Augustine <eriq.augustine@gmail.com>
2025-08-17 16:26:24 -0700

    Working on building the new documentation site.

--- pacai/__init__.py
+++ pacai/__init__.py
@@ -1,7 +1,5 @@
 """
-The pacai package is the top-level package for the Pacman AI project maintained by the LINQS
-lab at UCSC.
-This project originally came from the Berkeley AI Lab.
+The pacai package is the top-level package for the Pacman AI project maintained by EduLinq, Eriq Augustine, and the LINQS lab at UCSC.
 """
 
 __version__ = '2.0.0'
