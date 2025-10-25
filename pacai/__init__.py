203322bf1c6d1545aa64c5dd59f7728e41e3a751
BatuhanSA <basalihcs@gmail.com>
2025-10-25 13:03:14 -0700

    Version up.

--- pacai/__init__.py
+++ pacai/__init__.py
@@ -2,4 +2,4 @@
 The pacai package is the top-level package for the Pacman AI project maintained by EduLinq, Eriq Augustine, and the LINQS lab at UCSC.
 """
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
